18059717887cdcccca40637136f4532311672465
NekaDavaa <pavlov@webtik.bg>
2023-04-05 21:19:28 +0300

    d

---
+++ wp-content/cache/acorn/framework/views/22f16e6089f87021b08e9514a75be6ba8b5167eb.php
@@ -0,0 +1,5 @@
+<div <?php echo e($attributes->merge(['class' => $type])); ?>>
+  <?php echo $message ?? $slot; ?>
+
+</div>
+<?php /**PATH C:\laragon\www\BalkanBurrito\wp-content\themes\WEBTIK.BG\resources\views/components/alert.blade.php ENDPATH**/ ?>
---
+++ wp-content/cache/acorn/framework/views/7aaf26eaf9aab65d45e15475acfb3c85ddbed3a4.php
@@ -0,0 +1,2 @@
+<?php (dynamic_sidebar('sidebar-primary')); ?>
+<?php /**PATH C:\laragon\www\BalkanBurrito\wp-content\themes\WEBTIK.BG\resources\views/sections/sidebar.blade.php ENDPATH**/ ?>
