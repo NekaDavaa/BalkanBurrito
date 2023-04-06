ec59ca501df2e8f5a2c38360c9beecf6ad0306d0
NekaDavaa <pavlov@webtik.bg>
2023-04-06 10:25:25 +0300

    a

---
+++ wp-content/cache/acorn/framework/views/acfcf5d00fe50eee1d921ad9a3119b3f-loader.php
@@ -0,0 +1,2 @@
+<?= \Roots\view('partials.comments', $data ?? get_defined_vars())->render(); ?>
+<?php /**PATH C:\laragon\www\BalkanBurrito\wp-content\themes\balkanburrito\resources\views/partials/comments.blade.php ENDPATH**/ ?>
